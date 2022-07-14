100d300021441a97f96d2c563b9400e3d4dfaec1
KIM TAE HOON <naxoruby@gmail.com>
2022-07-14 23:49:20 +0900

    Update anim3.cs
    Coroutine 을 활용해서 한번 눌러서 각도 n 회전은 가능하다. 그런데 여러번 눌러서 계속 돌아가게 하는 구현이 안된다.

--- Assets/anim3.cs
+++ Assets/anim3.cs
@@ -13,12 +13,37 @@ public class anim3 : MonoBehaviour
         
     }
 
+    public void X90()
+    {
+        Vector3 nowRotation = gameObject.transform.rotation.eulerAngles;
+
+        if(gameObject.transform.rotation.eulerAngles.x <
+            Quaternion.Euler(target).eulerAngles.x)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(
+                nowRotation + new Vector3(1, 0, 0));
+        } else
+        {
+            StopCoroutine("RotateX90");
+        }
+        //gameObject.transform.rotation.eulerAngles.x == Quaternion.Euler(target).eulerAngles.x
+
+    }
+
+    IEnumerator RotateX90()
+    {
+        for(; ; )
+        {
+            X90();
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            gameObject.transform.rotation = Quaternion.Euler(target);
+            StartCoroutine("RotateX90");
         }
     }
 }
