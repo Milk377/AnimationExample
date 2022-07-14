5d4d6fa6f5861217b8fb5e3f305a2b39724dd43c
KIM TAE HOON <naxoruby@gmail.com>
2022-07-14 22:54:08 +0900

    add func
    minor fix

--- Assets/anim2.cs
+++ Assets/anim2.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class anim2 : MonoBehaviour
 {
+    Vector3 targetRotation;
+
     public void Rotate1()
     {
         // 90에 도달하는 순간 끝인데 계속 도니까 뻑뻑한다.
+        
         Vector3 nowRotation = gameObject.transform.rotation.eulerAngles;
-        gameObject.transform.rotation = Quaternion.Euler(nowRotation + new Vector3(1, 0, 0));
+        gameObject.transform.rotation = Quaternion.Euler(nowRotation + new Vector3(2, 0, 0));
     }
 
     IEnumerator DoRotate1()
@@ -16,7 +19,7 @@ public class anim2 : MonoBehaviour
         for (; ; )
         {
             Rotate1();
-            yield return new WaitForSeconds(.1f);
+            yield return new WaitForSeconds(.01f);
         }
     }
 
@@ -24,7 +27,8 @@ public class anim2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetRotation = gameObject.transform.rotation.eulerAngles + new Vector3(90,0,0);
+
     }
 
     // Update is called once per frame
